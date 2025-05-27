af5d676be9b9209c83b2a7f9864624b2ffa902b1
Michal Skoda <michal@Michal-MacBook-Air-3.local>
2025-05-27 23:14:18 +0200

    BubleSort.rb

---
+++ bubleSort.rb
@@ -0,0 +1,18 @@
+stock_picker = [17,3,6,9,15,8,6,1,10]
+
+array = [1,17,16,15,14,13,12,11,10,2,100]
+
+
+loop do
+	sorted = false
+		(array.count-1).times { |i| 
+			if array[i] > array[i+1]
+				array[i], array[i + 1] = array[i + 1], array[i]
+				sorted = true
+			end
+			print array
+			puts ""
+
+		}
+	break unless sorted
+end
--- stockPrice.rb
+++ stockPrice.rb
@@ -7,6 +7,7 @@ buy_day = 0
 
 stock_picker.each_with_index do |buy_price, buy_index|
 	stock_picker[buy_index + 1..].each_with_index do |sell_price, sell_index|
+		puts "#{sell_index} - #{buy_index}"
 		sell_index = buy_index + 1 + sell_index
 		if sell_price - buy_price > stock_picker[sell_day] - stock_picker[buy_day]
 			sell_day = sell_index
